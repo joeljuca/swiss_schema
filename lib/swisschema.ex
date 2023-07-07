ba9d0b13f5ddec150d76d1344e3b0882f82aada5
Joel Jucá <joelwallis@gmail.com>
2023-07-07 18:42:43 -0300

    chore: `mix new swisschema`

---
+++ lib/swisschema.ex
@@ -0,0 +1,18 @@
+defmodule Swisschema do
+  @moduledoc """
+  Documentation for `Swisschema`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Swisschema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/swisschema_test.exs
@@ -0,0 +1,8 @@
+defmodule SwisschemaTest do
+  use ExUnit.Case
+  doctest Swisschema
+
+  test "greets the world" do
+    assert Swisschema.hello() == :world
+  end
+end
