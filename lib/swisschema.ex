3c7443405819d727fec17b726f9882206fce6d71
Joel Jucá <joelwallis@gmail.com>
2023-07-07 12:33:22 -0300

    chore: rm hello/0

--- lib/swisschema.ex
+++ lib/swisschema.ex
@@ -2,17 +2,4 @@ defmodule Swisschema do
   @moduledoc """
   Documentation for `Swisschema`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Swisschema.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/swisschema_test.exs
+++ test/swisschema_test.exs
@@ -1,8 +1,4 @@
 defmodule SwisschemaTest do
   use ExUnit.Case
   doctest Swisschema
-
-  test "greets the world" do
-    assert Swisschema.hello() == :world
-  end
 end
